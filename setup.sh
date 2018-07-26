54f7528e22e4883c376e55aa365bdcd74afc2583
youdeee <youdee.t.ud@gmail.com>
2018-07-27 00:15:58 +0900

    add: ergodox_keymaps

--- setup.sh
+++ setup.sh
@@ -5,5 +5,6 @@ ln -sf ~/dotfiles/shell/.zshrc ~/.zshrc
 ln -sf ~/dotfiles/shell/.tmux.conf ~/.tmux.conf
 ln -sf ~/dotfiles/shell/.irbrc ~/.irbrc
 ln -sf ~/dotfiles/shell/.vimrc ~/.vimrc
+ln -sf ~/dotfiles/ergodox_keymaps/youdee ~/qmk_firmware/keyboards/ergodox_ez/keymaps/youdee
 
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
